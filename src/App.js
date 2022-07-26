dc471378a68727b1ed1446879d600c271267a945
Guilherme Ferreira <guilherme.fs.db@gmail.com>
2022-07-26 13:26:35 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import NavyBar from './components/SideBar/NavyBar/NavyBar.js';
+import SideBar from './components/SideBar/SideBar.js'
+
+function App() {
+  return (<>
+    <NavyBar />
+    <SideBar />
+  </>)
+}
+
+export default App;
---
+++ src/components/SideBar/NavyBar/NavyBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './NavyBar.css'
+
+const NavyBar = () => {
+    return (
+        <div className='navyBar'>
+            <div className='content'>
+                <button>Menu</button>
+                <label>Usuário - Nome da campanha - Data</label>
+                <div className='circle'></div>
+            </div>
+        </div>
+    )
+}
+
+export default NavyBar
